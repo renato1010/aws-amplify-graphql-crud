4ef9fedd2756a1c71c1136d595476cd96b4d7fce
Renato Perez <renato.perez@wekynect.com>
2021-03-05 20:28:46 -0600

    Create GraphQL Api based on model

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
